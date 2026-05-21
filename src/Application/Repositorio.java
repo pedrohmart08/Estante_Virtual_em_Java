0ed61e9705ab5c5cb7988e9162d02f232c6b49ae
pedrohmart08 <pedrohmart08@gmail.com>
2026-05-21 01:53:31 -0300

    refactor/Classe repositorio refatorada

--- src/Application/Repositorio.java
+++ src/Application/Repositorio.java
@@ -8,87 +8,73 @@ public class Repositorio {
 	private int contadorId = 1;
 	private List<Livro> bd = new ArrayList<>();
 	
-//	adiciona obras ao bd
+
 	public void create(Livro l) {
 		l.setId(contadorId++);
 		bd.add(l);
 	}
-//PRECISO REFATORAR ISSO
-//	Mostra todas as obras
-	public void read() {
+	
+	public String read() {
 		if(bd.isEmpty()) {
-			System.out.println("Não há livros registrados. ");
+			return "Não há livros registrados. \n";
 		}else {
-			for(int i = 0; i < bd.size(); i++) {
-				System.out.println(bd.get(i));
+			for(Livro l: bd) {
+				return l.toString();
 			}
 		}
-		
+		return "";
 	}
-//	Procura por titulo
-	public void buscarPorTitulo(String t) {
-	    boolean encontrado = false;
-	    for (Livro livro : bd) {
-	        if (livro.getTitulo().equalsIgnoreCase(t)) {
-	            System.out.println(livro);
-	            encontrado = true;
+
+	public String buscarPorTitulo(String t) {
+	    for (Livro l : bd) {
+	        if (l.getTitulo().equalsIgnoreCase(t)) {
+	            return l.toString();     
 	        }
 	    }
-	    if (!encontrado) {
-	        System.out.println("Não há livros registrados com esse título.");
-	    }
+	    return "Livro não encontrado.\n";
 	}
-//	Procura por autor
-	public void buscarPorAutor(String a) {
-	    boolean encontrado = false;
-	    for (Livro livro : bd) {
-	        if (livro.getAutor().equalsIgnoreCase(a)) {
-	            System.out.println(livro);
-	            encontrado = true;
+
+	public String buscarPorAutor(String a) {
+	    for (Livro l : bd) {
+	        if (l.getAutor().equalsIgnoreCase(a)) {
+	            return l.toString();
 	        }
 	    }
-	    if (!encontrado) {
-	        System.out.println("Não há livros registrados com esse autor.");
-	    }
+	    return "Não há livros registrados com esse autor.\n";
 	}
-//	procura por id
-	public void buscarPorId(int id) {
-		for(int i = 0; i < bd.size(); i++) {
-			if(bd.get(i).getId() == id) {
-				System.out.println(bd.get(i));
+	
+	public String buscarPorId(int id) {
+		for(Livro l: bd) {
+			if(l.getId() == id) {
+				return l.toString();
+			}
+		}
+		return "Não há livros registrados com esse autor.\n";
+	}
+	public void update(int id, String a, String t, boolean lido) {
+		for(Livro l: bd) {
+			if(l.getId() == id) {
+				l.setAutor(a);
+				l.setTitulo(t);
+				l.setLido(lido);
 				return;
 			}
 		}
-		System.out.println("Não há livros registrados com esse endereço. ");
 	}
-	public void update(int id, String a, String t, boolean l) {
-		for(int i = 0; i < bd.size(); i++) {
-			if(bd.get(i).getId() == id) {
-				bd.get(i).setAutor(a);
-				bd.get(i).setTitulo(t);
-				bd.get(i).setLido(l);
-				return;
-			}
-		}
-		System.out.println("Não foi possivel atualizar. ");
-	}
-	/* só podemos deletar pelo id facilita no códigokk */
 	public void delete(int id) {
-		for(int i = 0; i < bd.size(); i++) {
-			if(bd.get(i).getId() == id) {
-				bd.remove(i);
+		for(Livro l : bd) {
+			if(l.getId() == id) {
+				bd.remove(l);
 				return;
 			}
 		}
-		System.out.println("Não foi possivel remover, id não encontrado");
 	}
 	public void delete(String t) {
-		for(int i = 0; i < bd.size(); i++) {
-			if(bd.get(i).getTitulo().equalsIgnoreCase(t)) {
-				bd.remove(i);
+		for(Livro l: bd) {
+			if(l.getTitulo().equalsIgnoreCase(t)) {
+				bd.remove(l);
 				return;
 			}
 		}
-		System.out.println("Não foi possivel remover, titulo não encontrado");
 	}
 }
